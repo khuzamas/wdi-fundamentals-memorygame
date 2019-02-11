ef2524bf72e441f920394c49cea42f4bf5ce77e7
Khuzam <KhuzamShubbar@Khuzam-2.local>
2019-02-11 10:14:02 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,20 @@
-/*
-var cardThree= "king";
-var cardThree= "king";
-*/
-//alert('Hello, friends.');
 
 var cards= ["queen", "queen", "king", "king"];
 var cardsInPlay= [];
-var cardOne= cards[0];
-var cardTwo= cards[2];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-    } else {
-      alert("Sorry, try again.");
-    }
+var checkForMatch= function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 }
+
+var flipCard= function (cardID) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
