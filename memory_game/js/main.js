9eb147af0e680d033bd0a4ce6eb0f518df8fafd5
Khuzam <KhuzamShubbar@Khuzam-2.local>
2019-02-13 13:21:34 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,14 +24,23 @@ var cards= [
 var cardsInPlay= [];
 
 var checkForMatch= function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
-  } else {
-    console.log("Sorry, try again.");
+  try {
+    this.setAttribute('src', this.cardImage);
   }
+  catch(err) {
+  }
+
+
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+
 }
 
-var flipCard= function (cardID) {
+var flipCard= function () {
+  var cardID= this.getAttribute('data-id');
   console.log("User flipped " +
     cards[cardID].rank);
   cardsInPlay.push(cards[cardID].suit);
@@ -40,5 +49,15 @@ var flipCard= function (cardID) {
   console.log(cards[cardID].suit);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard= function() {
+  for (var i= 0; i < cards.length; i++) {
+    var cardElement= document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById('game-board')
+    .appendChild(cardElement);
+  }
+}
+
+createBoard();
